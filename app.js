155ef1ee49cc7b50eb3b0e6aace04f210c94e83c
Shashi Shekhar <Shekhars094@gmail.com>
2020-04-26 01:15:06 +0530

    Found Problem

--- app.js
+++ app.js
@@ -13,15 +13,15 @@ const port = process.env.PORT || 5000;
 const userRouter = require("./controllers/auth");
 const postRouter = require("./controllers/post");
 
-app.get("/", (req, res) => {
-	res.json((message: "Everything is fine"));
-});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1", userRouter);
 app.use("/api/v1", postRouter);
+
+app.get("/", (req, res) => {
+	res.json((message: "Everything is fine"));
+});
 
 app.listen(port, () => {
 	console.log("App is running on ", port);
